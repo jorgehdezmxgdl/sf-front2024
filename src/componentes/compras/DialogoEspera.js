bdeada09950c7f4973ca436bb02254812f986d6f
jorgehdezmxgdl <67309059+jorgehdezmxgdl@users.noreply.github.com>
2024-06-12 23:12:30 -0600

    compras

--- src/componentes/compras/DialogoEspera.js
+++ src/componentes/compras/DialogoEspera.js
@@ -1,18 +1,47 @@
-import { Dialog, DialogContent, CircularProgress, Typography } from '@mui/material';
+import {
+  Dialog,
+  DialogContent,
+  Typography,
+} from "@mui/material";
+
+import './espera.css';
 
 const DialogoEspera = ({ open }) => {
   return (
     <Dialog
       open={open}
+      PaperProps={{
+          sx: {
+            width: 300,
+            height: 300,
+          },
+        }}
       aria-labelledby="loading-dialog"
       disableBackdropClick
       disableEscapeKeyDown
     >
-      <DialogContent style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-        <CircularProgress />
-        <Typography variant="body1" style={{ marginTop: '16px' }}>
-            Procesando la información.....
-        </Typography>
+      <DialogContent
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          backgroundColor: "#212F3C"
+        }}
+      >
+        <div id="page">
+          <div id="container">
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="h3">
+              <Typography variant="body1" style={{ marginTop: "13px" }}>
+                Espere un momento...
+              </Typography>
+            </div>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
